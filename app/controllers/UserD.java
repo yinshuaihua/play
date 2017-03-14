17e33ea4352d4dd52205d4c5e8f5702a4a9858f0
YSH <shuaihua_93@outlook.com>
2017-03-14 16:39:07 +0800

    登录功能暂存

--- app/controllers/UserD.java
+++ app/controllers/UserD.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import play.Logger;
 import play.data.DynamicForm;
 import play.data.Form;
 import play.libs.Json;
@@ -29,14 +30,17 @@ public class UserD extends Controller {
         return ok(res);//返回给请求页面res变量
     }
 
+    //用户注册接口
     public Result userRegister(){
         DynamicForm requestData = Form.form().bindFromRequest();
-        String userName = requestData.get("userName");
+        String nickname = requestData.get("nickname");
         String userEmail = requestData.get("userEmail");
         String userPassword = requestData.get("userPassword");
+        Logger.debug("regist test"+nickname+userEmail+userPassword);
+
         ObjectNode res = Json.newObject();
         models.UserD registerUser = new models.UserD();//新建一个数据库models的userD对象，名字叫registerUser
-        registerUser.setUserName(userName);//添加用户昵称
+        registerUser.setUserName(nickname);//添加用户昵称
         registerUser.setUserEmail(userEmail);//添加用户邮箱
         registerUser.setUserPassword(userPassword);//添加用户密码
         registerUser.save();
@@ -45,6 +49,11 @@ public class UserD extends Controller {
 
     //用户注册页面
     public Result registerPage(){
+        return ok(views.html.registerPage.render());
+    }
+
+    //登录接口
+    public Result login(){
         return ok(views.html.registerPage.render());
     }
 
